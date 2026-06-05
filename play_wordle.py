1fe5edb9d02a82c104185234176f1251710c6119
SandeepThapaChhetri <164916035+SandeepThapaChhetri@users.noreply.github.com>
2026-06-05 07:17:10 +0545

    Few changes have been made

--- play_wordle.py
+++ play_wordle.py
@@ -2,8 +2,17 @@ from wordle import Wordle
 
 def main():
   print("Hello World!")
-  wordle = Wordle()
-  print(wordle)
+  wordle = Wordle("APPLE")
+  
+  while wordle.can_attempt:
+    x = input("Type your guess:")
+    wordle.attempts.append(x)
+    if x == wordle.secret:
+      print("You have guessed the word!")
+      break
+    print("Your guess is incorrect.")
+
+
 
 if __name__ == "__main__":
   main()
--- wordle.py
+++ wordle.py
@@ -1,4 +1,24 @@
 class Wordle:
 
-  def __init__(self):
+  MAX_ATTEMPTS = 6
+  WORD_LENGTH = 5
+
+  def __init__(self, secret: str):
+    self.secret: str = secret
+    self.attempts
     pass
+
+  def attempt(self, )
+
+  @property
+  def is_solved(self):
+    return self.attempts[-1] == self.secret
+  
+  @property
+  def remaining_attempts(self) -> int:
+    return self.MAX_ATTEMPTS - len(self.attempts)
+
+  @property
+  def can_attempt(self):
+    return  self.remaining_attempts == 0and not self.is_solved
+    pass 
